2080a215fca230a6e868df7cc0527984738cd887
sayed3li97 <alkamel331@gmail.com>
2019-10-22 22:54:03 +0300

    qr changes

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,9 +2,9 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/sayedalialkamel/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/sayedalialkamel/Documents/GitHub/GDG-DevFest-App"
-export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_TARGET=/Users/sayedalialkamel/Documents/GitHub/GDG-DevFest-App/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/sayedalialkamel/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_FRAMEWORK_DIR=/Users/sayedalialkamel/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=1.0.4"
 export "FLUTTER_BUILD_NUMBER=5"
